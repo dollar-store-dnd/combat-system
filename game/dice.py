d80831bee3a86848d02a75e96f8e4da0562e964d
TravisZehring <tzehring000@gmail.com>
2020-08-09 17:06:34 -0400

    Added Basic Combat Loop

--- game/dice.py
+++ game/dice.py
@@ -1,6 +1,7 @@
 from game.equipment import Damage
 
 from random import randint
+D20_DICE = Damage(die_type=20, dice=1, type="")
 
 
 def roll_dice(damage: Damage) -> int:
--- run.py
+++ run.py
@@ -1,18 +1,112 @@
 from collections import deque
 
+from dataclasses import dataclass, field
 from game import armor, weapons
 from game.dice import roll_dice
 from game.loader import load_game_assets
+from game.dice import D20_DICE
+from pprint import pprint
+from game.equipment import Damage
+from time import sleep
+
+load_game_assets()
+
+
+@dataclass
+class Enemy:
+    health = 20
+    weapon = weapons[0]
+    name = "Steven Seagal"
+
+
+@dataclass
+class PlayerCharacter:
+    player_name: str = "Joe Malone"
+    name: str = "Moe Jalone"
+    health: int = 20
+    weapon = weapons[0]
+
+
+def print_characters(character: PlayerCharacter, enemy: Enemy):
+    print("#" * 64)
+    print("PLAYER:\n", character.name, "HP:", character.health)
+    print("ENEMY:\n", enemy.name, "HP:", enemy.health)
+    print("#" * 64)
+
+
+def print_roll(roll: int):
+    print("Rolled:", roll)
 
 
 def start_combat() -> None:
     """Basic combat loop"""
     turn_queue = deque()
 
+    main_char = PlayerCharacter()
+    enemy_char = Enemy()
+
+    player_roll = roll_dice(damage=D20_DICE)
+    enemy_roll = roll_dice(damage=D20_DICE)
+
+    while player_roll == enemy_roll:
+        player_roll = roll_dice(damage=D20_DICE)
+        enemy_roll = roll_dice(damage=D20_DICE)
+
+    if player_roll > enemy_roll:
+        turn_queue.appendleft(main_char)
+        turn_queue.appendleft(enemy_char)
+    else:
+        turn_queue.appendleft(enemy_char)
+        turn_queue.appendleft(main_char)
+
+    while True:
+        character = turn_queue[-1]
+        turn_queue.rotate()
+
+        if isinstance(character, PlayerCharacter):
+            print_characters(main_char, enemy_char)
+            print("Your turn:")
+            action = input("- Attack   - Heal \n")
+
+            if action == "Attack":
+                hit = roll_dice(damage=D20_DICE)
+                print_roll(hit)
+
+                if hit >= 15:
+                    damage = roll_dice(character.weapon.damage)
+                    enemy_char.health -= damage
+                    print("Enemy took:", damage, "damage!")
+                    print("End turn!")
+                    if enemy_char.health <= 0:
+                        print(main_char.name, "WINS!!!")
+                        break
+
+                else:
+                    print(main_char.name, "MISSED!")
+
+            elif action == "Heal":
+                heal = roll_dice(damage=Damage(die_type=10, dice=1, type=""))
+                main_char.health += heal
+                print("Healed for:", heal)
+
+        else:
+            hit = roll_dice(damage=D20_DICE)
+
+            if hit >= 15:
+                damage = roll_dice(character.weapon.damage)
+                main_char.health -= damage
+                print("You took:", damage, "damage!")
+
+                if main_char.health <= 0:
+                    print("YOU LOSE!!!")
+                    break
+            else:
+                print(enemy_char.name, "MISSED!")
+
+        sleep(1)
+
     # TODO(TravisZehring): Write code here
-    pass
 
 
 if __name__ == "__main__":
-    load_game_assets()
     start_combat()
